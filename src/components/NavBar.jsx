860667826a616002ae1214964ac2fadd21cee580
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2023-10-30 11:25:26 -0700

    Starting work on the NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+const names = [
+    {line1: 'Brandon', line2: 'Black'},
+    {line1: 'Yet Another', line2: 'Portfolio Site'},
+    {line1: 'Brandon', line2: 'Black.dev'},
+    {line1: 'Yet Another', line2: 'Seattle Developer'},
+]
+
+export default function NavBar() {
+    const [line1, setLine1] = useState('Brandon');
+    const [line2, setLine2] = useState('Black');
+
+    function changeName() {
+        let index = Math.floor(Math.random() * 4)
+        if (line1 === names[index].line1) {
+            if (index >= names.length - 1) {
+                index = 0;
+            } else {
+                index++;
+            }
+        }
+        setLine1(names[index].line1);
+        setLine2(names[index].line2);
+    }
+
+    return (
+        <nav className='bg-primary'>
+            <div onMouseEnter={() => changeName()}>
+                <p>{line1}</p>
+                <p>{line2}</p>
+            </div>
+        </nav>
+    )
+}
